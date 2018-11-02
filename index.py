6569cb220c889c8fc8a0c3cc50efa2413da5d2e6
TheRealToast <33233103+TheRealPi@users.noreply.github.com>
2018-11-02 09:32:21 -0500

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+print("Please insert your username and your password")
+
+username = input("Username:")
+
+password = input("Password")
+
+if username = Toast:
+  return;
+  
+if password = insertPasswordHere"
